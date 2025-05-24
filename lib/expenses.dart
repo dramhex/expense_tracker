d196c24e78a5bd971046dbbc1152f63877c2c3a6
dramhex <92268356+dramhex@users.noreply.github.com>
2025-05-24 06:41:38 +0200

    Add ExpensesList widget and implement expense tracking functionality

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -1,3 +1,5 @@
+import 'package:expense_tracker/models/expense.dart';
+import 'package:expense_tracker/expenses_list.dart';
 import 'package:flutter/material.dart';
 
 class Expenses extends StatefulWidget {
@@ -10,15 +12,22 @@ class Expenses extends StatefulWidget {
 }
 
 class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(title: 'Water', amount: 2, date: DateTime.now(), category: Category.food),
+    Expense(title: 'Cinema', amount: 15, date: DateTime.now(), category: Category.leisure)
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(
-      child: Column(
-        children: const [
-          Text('The chart'),
-          Text('Expenses list')
-        ],
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            const Text('The chart'), 
+            Expanded(child: ExpensesList(expenses: _registeredExpenses))
+            ],
+        ),
       ),
-    ),);
+    );
   }
 }
